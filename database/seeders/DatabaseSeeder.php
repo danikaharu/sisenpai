497eaf3c4c4c6e0e86a07665dac05b0897996be5
danikaharu <dani.kaharu17@gmail.com>
2023-01-18 11:50:44 +0800

    add agency, position and employee seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            AgencyTableSeeder::class,
+            PositionTableSeeder::class,
+            EmployeeTableSeeder::class,
             UserTableSeeder::class,
             RoleAndPermissionTableSeeder::class,
         ]);
