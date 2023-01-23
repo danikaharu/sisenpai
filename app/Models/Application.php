59b456cf5d6e31873eb18dcaad79923a0407b2e9
danikaharu <dani.kaharu17@gmail.com>
2023-01-23 23:43:23 +0800

    add status and type scope

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -11,6 +11,28 @@ class Application extends Model
 
     protected $fillable = ['user_id', 'type', 'explanation', 'file', 'start_date', 'end_date', 'status'];
 
+    public function type()
+    {
+        if ($this->type == 1) {
+            return 'Izin';
+        } elseif ($this->type == 2) {
+            return 'Cuti';
+        } else {
+            return 'Sakit';
+        }
+    }
+
+    public function status()
+    {
+        if ($this->status == 1) {
+            return 'Disetujui';
+        } elseif ($this->status == 2) {
+            return 'Belum disetujui';
+        } else {
+            return 'Ditolak';
+        }
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class)->withDefault();
