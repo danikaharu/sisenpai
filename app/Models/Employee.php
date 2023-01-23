e1e13d9ace90a2f5d4344d5d5b120a7caeef0e61
danikaharu <dani.kaharu17@gmail.com>
2023-01-23 22:29:32 +0800

    add user relation

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -20,4 +20,9 @@ class Employee extends Model
     {
         return $this->belongsTo(Position::class)->withDefault();
     }
+
+    public function user()
+    {
+        return $this->hasOne(User::class)->withDefault();
+    }
 }
