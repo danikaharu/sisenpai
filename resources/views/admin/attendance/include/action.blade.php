2344a8a394d72b84eae8cb8cd28f6bd26d3cf441
danikaharu <dani.kaharu17@gmail.com>
2023-01-23 23:42:44 +0800

    add attendance view

---
+++ resources/views/admin/attendance/include/action.blade.php
@@ -0,0 +1,5 @@
+<div class="flex">
+    <a class="btn btn-warning me-2" href="{{ asset('storage/upload/absen/' . $photo) }}" target="_blank">
+        <i class="bi bi-eye-fill me-1"></i>
+    </a>
+</div>
