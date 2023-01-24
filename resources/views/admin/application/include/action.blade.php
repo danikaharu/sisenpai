141a61eddc5e05010888ff98e38654a27c378c1e
danikaharu <dani.kaharu17@gmail.com>
2023-01-24 21:02:37 +0800

    add approve button

--- resources/views/admin/application/include/action.blade.php
+++ resources/views/admin/application/include/action.blade.php
@@ -5,19 +5,27 @@
             Detail</a>
     @endcan
 
-    @can('edit application')
-        <a class="btn btn-warning btn-sm me-2" href="{{ route('application.edit', $id) }}">
-            Edit</a>
-    @endcan
+    @if ($status == 2)
+        @can('edit application')
+            <a class="btn btn-warning btn-sm me-2 mt-2" href="{{ route('application.edit', $id) }}">
+                Edit</a>
+        @endcan
 
-    @can('delete application')
-        <form action="{{ route('application.destroy', $id) }}" method="POST" role="alert" alert-title="Hapus pengajuan"
-            alert-text="Yakin ingin menghapusnya?">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger btn-sm me-2 mt-2">
-                Hapus</button>
-        </form>
-    @endcan
+        @can('approve application')
+            <a class="btn btn-secondary btn-sm me-2 mt-2" href="{{ route('application.approve', $id) }}">
+                Proses</a>
+        @endcan
+
+
+        @can('delete application')
+            <form action="{{ route('application.destroy', $id) }}" method="POST" role="alert" alert-title="Hapus pengajuan"
+                alert-text="Yakin ingin menghapusnya?">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger btn-sm me-2 mt-2">
+                    Hapus</button>
+            </form>
+        @endcan
+    @endif
 
 </div>
