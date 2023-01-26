c169fd0b7451a362a7b9e2a7e802772302a7eb53
danikaharu <dani.kaharu17@gmail.com>
2023-01-26 21:28:33 +0800

    edit dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,9 +11,11 @@ class DashboardController extends Controller
     public function index()
     {
         $todayAttendance = Attendance::whereDate('created_at', Carbon::today())->latest()->get();
-        $checkinAttendance = Attendance::checkAttendance(1)->first();
-        $checkoutAttendance = Attendance::checkAttendance(2)->first();
+        $checkinRegular = Attendance::checkAttendance(1)->first();
+        $checkoutRegular = Attendance::checkAttendance(2)->first();
+        $checkinAssignment = Attendance::checkAttendance(3)->first();
+        $checkoutAssignment = Attendance::checkAttendance(4)->first();
 
-        return view('admin.dashboard.index', compact('todayAttendance', 'checkinAttendance', 'checkoutAttendance'));
+        return view('admin.dashboard.index', compact('todayAttendance', 'checkinRegular', 'checkoutRegular', 'checkinAssignment', 'checkoutAssignment'));
     }
 }
