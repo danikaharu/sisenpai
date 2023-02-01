349d61db211125d0e905af1155dda56ed6647ddb
danikaharu <dani.kaharu17@gmail.com>
2023-02-01 11:36:41 +0800

    edit model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -9,34 +9,31 @@ class Attendance extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'longitude', 'latitude', 'time', 'photo'];
+    protected $fillable = ['user_id', 'type', 'checkin_longitude', 'checkin_latitude', 'checkout_longitude', 'checkout_latitude', 'checkin_time', 'checkout_time', 'checkin_photo', 'checkout_photo'];
 
     public function user()
     {
         return $this->belongsTo(User::class)->withDefault();
     }
 
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class)->withDefault();
+    }
+
     public function type()
     {
         if ($this->type == 1) {
-            return 'Absen Masuk';
-        } elseif ($this->type == 2) {
-            return 'Absen Pulang';
-        } elseif ($this->type == 3) {
-            return 'Absen Penugasan Masuk';
+            return 'Reguler';
         } else {
-            return 'Absen Penugasan Pulang';
+            return 'Penugasan';
         }
     }
 
     public function status()
     {
-        if ($this->type == 1 || $this->type == 3) {
-            if ($this->time > '09:00:00') {
-                return 'Terlambat';
-            } else {
-                return 'Tepat Waktu';
-            }
+        if ($this->checkin_time > '09:00:00') {
+            return 'Terlambat';
         } else {
             return 'Tepat Waktu';
         }
